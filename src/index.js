62c86a2f50e11701df909a4bdaa3d57326c911fa
SalathielGenese <salathielgenese@gmail.com>
2021-06-19 17:05:10 +0400

    feat: configure and register body-parser to express

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@
  * @author Salathiel &lt;salathiel@genese.name&gt;
  *
  */
+const bodyParser = require( 'body-parser' );
 const express = require( 'express' );
 const { PORT } = require( './env' );
+
 const app = express();
+
+app.use( bodyParser.urlencoded( { extended: false } ) );
+app.use( bodyParser.json() );
 
 app.use( ( req, res ) => res.json( {
     method: req.method,
